76fb632ef909a409299af58b9d9bd1d8b6f1200a
YoongTi Lee <leeyoongti@outlook.com>
2018-07-14 23:34:06 +0800

    added dummy routes for login, register, and board

---
+++ client/src/components/home/Board.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Board extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Board Game</h1>
+      </div>
+    );
+  }
+}
+
+export default Board;
