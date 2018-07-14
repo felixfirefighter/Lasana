ae0b0fde4cdf33c92b661d4709ba43e24e129b65
YoongTi Lee <leeyoongti@gmail.com>
2018-07-14 23:44:41 +0800

    change port to 8000

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/passport")(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Listening to port ${port}`));
