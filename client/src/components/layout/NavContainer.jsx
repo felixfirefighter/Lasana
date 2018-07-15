266b5c7b9e08e9705714975582b4f7e61e470f0e
YoongTi Lee <leeyoongti@gmail.com>
2018-07-15 22:40:12 +0800

    added NavContainer

---
+++ client/src/components/layout/NavContainer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Sidebar,
+  Segment,
+  Menu,
+  Header,
+  Icon,
+  Dropdown,
+  Button
+} from "semantic-ui-react";
+
+import { connect } from "react-redux";
+
+import * as actions from "../../actions/authActions";
+
+const trigger = <Button icon="user" circular />;
+
+const options = [
+  { key: "sign-out", text: "Sign Out", icon: "sign out", value: "sign-out" }
+];
+
+const PushableSidebar = ({ children, visible, logout }) => {
+  this.handleChange = (e, { value }) => {
+    if (value === "sign-out") logout();
+  };
+
+  return (
+    <Sidebar.Pushable as={Segment}>
+      <Sidebar
+        as={Menu}
+        animation="push"
+        icon="labeled"
+        inverted
+        onHide={this.handleSidebarHide}
+        vertical
+        visible={visible}
+        width="thin"
+      >
+        <Menu.Item as="a">Home</Menu.Item>
+        <Menu.Item as="a">Games</Menu.Item>
+        <Menu.Item as="a">Channels</Menu.Item>
+      </Sidebar>
+
+      <Menu>
+        <Menu.Menu position="right">
+          <Dropdown
+            trigger={trigger}
+            options={options}
+            icon={null}
+            onChange={this.handleChange}
+          />
+        </Menu.Menu>
+      </Menu>
+      <Sidebar.Pusher>{children}</Sidebar.Pusher>
+    </Sidebar.Pushable>
+  );
+};
+
+export default connect(
+  null,
+  actions
+)(PushableSidebar);
