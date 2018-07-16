721745ecccfcde2f7651d84ce98d681dad6be97b
YoongTi Lee <leeyoongti@gmail.com>
2018-07-16 18:34:07 +0800

    added actions for get all projects and single projects for user

---
+++ client/src/actions/projectActions.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+import {
+  ADD_PROJECT,
+  UPDATE_PROJECT,
+  DELETE_PROJECT,
+  GET_PROJECT,
+  GET_PROJECTS,
+  GET_ERRORS
+} from "./types";
+
+export const getProjects = () => async dispatch => {
+  try {
+    const res = await axios.get("/api/projects");
+    dispatch({
+      type: GET_PROJECTS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
+
+export const getProject = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/projects/${id}`);
+    dispatch({
+      type: GET_PROJECT,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
