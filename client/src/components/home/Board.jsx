84090db39a501aa8dc2527fe1cbb7033c7e46df3
YoongTi Lee <leeyoongti@gmail.com>
2018-07-17 23:30:14 +0800

    cleanup

--- client/src/components/home/Board.jsx
+++ client/src/components/home/Board.jsx
@@ -1,32 +1,14 @@
 import React, { Component } from "react";
 
-import { Container, Segment, Header, Loader, Dimmer } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 
 import { connect } from "react-redux";
 
 class Board extends Component {
-  renderContent = () => {
-    const { project } = this.props;
-
-    return Object.keys(project.project).length === 0 ? (
-      <Dimmer active inverted>
-        <Loader inverted>Loading</Loader>
-      </Dimmer>
-    ) : (
-      <div>
-        <Segment>
-          <Header as="h1" textAlign="center">
-            {project.project.name}
-          </Header>
-        </Segment>
-      </div>
-    );
-  };
-
   render() {
     return (
       <Container fluid className="full-height">
-        {this.renderContent()}
+        <h1>Main Board</h1>
       </Container>
     );
   }
