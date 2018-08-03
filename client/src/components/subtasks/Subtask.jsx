6e7acdd71e01d12f241eda05a400da55e39843f6
YoongTi Lee <leeyoongti@gmail.com>
2018-08-03 20:00:26 +0800

    toggle between subtask input and text

--- client/src/components/subtasks/Subtask.jsx
+++ client/src/components/subtasks/Subtask.jsx
@@ -1,13 +1,48 @@
-import React from "react";
-import { List } from "semantic-ui-react";
+import React, { Component, Fragment } from "react";
+import { List, Input } from "semantic-ui-react";
 
-const Subtask = ({ name }) => {
-  return (
-    <List.Item>
-      <List.Icon size="small" name="check" circular className="pointer" />
-      <List.Content>{name}</List.Content>
-    </List.Item>
-  );
-};
+class Subtask extends Component {
+  state = {
+    active: false
+  };
+
+  handleClick = () => {
+    this.setState({ active: true });
+  };
+
+  handleBlur = () => {
+    // this.setState({ active: false });
+  };
+
+  render() {
+    const { name } = this.props;
+    const { active } = this.state;
+
+    return (
+      <Fragment>
+        <List.Item className="subtask" onClick={this.handleClick}>
+          <List.Icon
+            size="small"
+            name="check"
+            circular
+            className="complete-icon"
+          />
+
+          {active ? (
+            <Input
+              className="subtask-input"
+              size="mini"
+              fluid
+              autoFocus
+              onBlur={this.handleBlur}
+            />
+          ) : (
+            <List.Content>name</List.Content>
+          )}
+        </List.Item>
+      </Fragment>
+    );
+  }
+}
 
 export default Subtask;
