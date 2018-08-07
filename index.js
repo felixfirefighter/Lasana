7a8cfbe7aa3b83329b8e1c631064cbceb1fdae7e
YoongTi Lee <leeyoongti@gmail.com>
2018-08-07 23:58:10 +0800

    serve production asset

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/passport")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Listening to port ${port}`));
